e2815dab4d2d5b1b58f90f5c13db05e6574288b9
Mirko Campari <82829377+MaSTERmIKK@users.noreply.github.com>
2023-01-05 13:10:34 +0100

    PRIMODAY
    materiale e script del primo day

---
+++ Primoscript.py
@@ -0,0 +1,63 @@
+
+
+x = 12        # int || Integer || NUMERO INTERO
+
+x = "parola alfanumerica"   # String || Stringa || può contenere qualsiasi combinazione di lettere e numeri all'interno degli apici --> ""
+
+x = False     # Bool || Booleani || può contenere solo True o False
+
+x = 123.23434 # Numero cpomplesso FLOAT  può contenere numeri con al virgola fino ad una determinatA soglia
+
+# Semplici: interi (int), interi lunghi (long), numeri in virgola mobile (float)
+# numeri complessi (complex), valori booleani (bool)
+
+# Sequenze: immutabili: stringhe (str), tuple (tuple),
+# insiemi immutabili (frozenset) mutabili: liste (list), dizionari (dict)
+
+
+# Il paradigma di Py è OOP
+# I due livelli di tipizzazione (  DEBOLE come PY, FORTE come JAVA  )
+
+
+
+
+
+# Primi comandi " print  " e primi Operatori logici basilari ( =  e  == e != )
+
+
+# x = 25
+# y = "Master_D_MIRKO"
+# # scambiando i valori tutto funziona
+# x = y
+# y = 25
+# print(x)
+# print(y)
+
+# x = y
+
+# print ( x == y )
+# print ( x != y )
+
+
+
+
+
+
+# x = 4       
+# print(x)
+# X = "Master_D_Nuovo" 
+# print(X)
+# print(x)
+
+
+
+
+x = int(3)    # x ora sarà  '3'
+
+x2 = 3
+
+print ( x2 == x )   # il risultato sarà false
+
+
+y = int(1)    # y ora sarà  1
+z = float(9)  # z ora sarà 9.0
