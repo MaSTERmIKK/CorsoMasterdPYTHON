9844ce048cb6100dab13d198353908c1fd7ca31e
Mirko Campari <82829377+MaSTERmIKK@users.noreply.github.com>
2023-06-22 18:49:02 +0200

    Add files via upload

---
+++ Inizionuovocapitolo.py
@@ -0,0 +1,13 @@
+# Ecco un elenco di applicazioni comuni dell'astrazione in Python:
+
+# Incapsulamento: L'astrazione consente di incapsulare i dettagli interni di una classe, rendendoli privati o protetti. Ciò impedisce agli utenti esterni di accedere direttamente a tali dettagli, promuovendo una migliore organizzazione del codice e proteggendo l'integrità degli oggetti.
+
+# Gerarchia di classi: L'astrazione consente di creare classi astratte o interfacce che definiscono metodi comuni per un insieme di classi derivate. Questo permette di definire comportamenti generali che possono essere estesi o personalizzati dalle classi figlie.
+
+# Modularità: L'astrazione facilita la creazione di moduli che forniscono un'interfaccia coerente e isolano i dettagli di implementazione. Questo favorisce la riusabilità del codice e semplifica la manutenzione.
+
+# Polimorfismo: L'astrazione consente di utilizzare oggetti di diverse classi in modo uniforme attraverso l'interfaccia comune fornita dalla classe astratta o dall'interfaccia. Questo promuove la flessibilità e la sostituibilità degli oggetti.   
+
+#---------------------------------------  Rifare il contesto sopra con due figli per figlio e due metodi genralizati "poliformici" per entrambi
+
+
